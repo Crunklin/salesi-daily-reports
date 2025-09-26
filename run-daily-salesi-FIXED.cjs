a9dc957caa9d6809cdd7283fa29c0d06778ff697
Crunklin <cconklin86@aol.com>
2025-09-25 20:48:40 -0400

    Update and rename run-daily-salesi-FIXED_with-skip_early-check_panel-open.cjs to run-daily-salesi-FIXED.cjs

--- run-daily-salesi-FIXED.cjs
+++ run-daily-salesi-FIXED.cjs
@@ -15,10 +15,10 @@ const REPS = [
   { name: 'Murph',          id: '200226279' },
   { name: 'Jeremy',         id: '200224688' },
   { name: 'Jesse',          id: '200223301' },
-//  { name: 'John',           id: '200230324' },
+  { name: 'John',           id: '200230324' },
   { name: 'Kevin',          id: '200223210' },
-//  { name: 'Kevin Sellers',  id: '200229000' },
-//  { name: 'Matt Kartz',     id: '200228999' },
+  { name: 'Kevin Sellers',  id: '200229000' },
+  { name: 'Matt Kartz',     id: '200228999' },
   { name: 'Mike',           id: '200221589' },
   { name: 'Trevor',         id: '200227744' },
 ];
